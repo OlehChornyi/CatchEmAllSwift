dd161617a61e5956b45e924fa6b05500b8a95f9b
oleh <phd.chornyi@gmail.com>
2025-02-04 10:48:37 +0200

    Initial Commit

---
+++ CatchEmAllSwift/CatchEmAllSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllSwiftApp.swift
+//  CatchEmAllSwift
+//
+//  Created by Oleh on 04.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
